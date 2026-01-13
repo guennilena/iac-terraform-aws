3448ac9379c67047da0a93d61d1a819cf49ba40c
guennilena <git@guennilena.de>
2026-01-13 07:35:39 +0100

    Add remote state bootstrap (S3) and DynamoDB locking

--- providers.tf
+++ providers.tf
@@ -1,14 +1,18 @@
 terraform {
   required_version = ">= 1.5.0"
 
+  backend "s3" {
+    bucket         = "iac-terraform-aws-dev-state-20260113-0731"
+    key            = "iac-terraform-aws/dev/terraform.tfstate"
+    region         = "eu-central-1"
+    dynamodb_table = "terraform-state-lock"
+    encrypt        = true
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
   }
-}
-
-provider "aws" {
-  region = var.aws_region
 }
