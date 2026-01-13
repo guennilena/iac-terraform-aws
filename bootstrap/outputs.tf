d07e48387c7c6a7ef787898a6a7a4a07a9205d7e
guennilena <git@guennilena.de>
2026-01-13 08:02:32 +0100

    Expose bootstrap outputs for remote state resources

--- bootstrap/outputs.tf
+++ bootstrap/outputs.tf
@@ -1,9 +1,14 @@
-output "bucket_name" {
-  description = "Name of the created S3 bucket"
-  value       = aws_s3_bucket.this.bucket
+output "state_bucket_name" {
+  description = "Name of the S3 bucket used for Terraform remote state"
+  value       = aws_s3_bucket.tf_state.bucket
 }
 
-output "bucket_arn" {
-  description = "ARN of the created S3 bucket"
-  value       = aws_s3_bucket.this.arn
+output "state_bucket_arn" {
+  description = "ARN of the S3 bucket used for Terraform remote state"
+  value       = aws_s3_bucket.tf_state.arn
+}
+
+output "lock_table_name" {
+  description = "Name of the DynamoDB table used for Terraform state locking"
+  value       = aws_dynamodb_table.tf_lock.name
 }
